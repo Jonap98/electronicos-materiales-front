b8d1b20011a87cc8a4d4c797967515545cd2f4ae
Jona <=>
2023-09-06 11:37:35 -0600

    Creación del proyecto

---
+++ environments/environments.prod.ts
@@ -0,0 +1,6 @@
+export const environments = {
+  production: true,
+  urlBase: 'http://10.40.129.40:99/electronicos/api',
+  urlPage: 'http://10.40.129.40:99/electronicos/welcome/',
+  // urlImages: 'http://10.40.129.40:99/tkav/storage/'
+}
